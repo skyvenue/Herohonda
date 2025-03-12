e6f917a19ae9c89360e4a52b7c821f0d6c9f9910
anjal <anjal@ANCHAL>
2025-03-12 13:20:08 +0530

    Added randomNumber in java utility

--- myproject/src/main/java/com/javautlity/JavaUtlity.java
+++ myproject/src/main/java/com/javautlity/JavaUtlity.java
@@ -1,5 +1,11 @@
 package com.javautlity;
 
-public class JavaUtlity {
+import java.util.Random;
 
+public class JavaUtlity {
+public int getRandomNumber() {
+	Random rndm = new Random();
+	int Randomnum = rndm.nextInt(100);
+	return Randomnum;
+}
 }
