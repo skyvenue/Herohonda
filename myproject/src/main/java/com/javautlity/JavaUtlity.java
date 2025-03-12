9dd95b11b1b86181224fcf560f6a3670400c0383
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-12 13:19:26 +0530

    getsystemDate

--- myproject/src/main/java/com/javautlity/JavaUtlity.java
+++ myproject/src/main/java/com/javautlity/JavaUtlity.java
@@ -1,5 +1,17 @@
 package com.javautlity;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 public class JavaUtlity {
 
+	
+	public String getSystemDateYYYYMMDD() {
+		Date dateobj=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat("YYYY-MM-DD");
+		String StartDate=sdf.format(dateobj);
+		return StartDate;
+		
+	}
+	
 }
