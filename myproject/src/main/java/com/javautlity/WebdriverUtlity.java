d1531fc76e3340da2401b7ca75c8e3e201f43864
sumit <sumit@Sneh>
2025-03-12 12:45:13 +0530

    abc

--- myproject/src/main/java/com/javautlity/WebdriverUtlity.java
+++ myproject/src/main/java/com/javautlity/WebdriverUtlity.java
@@ -3,8 +3,8 @@ package com.javautlity;
 public class WebdriverUtlity {
 	public static void main(String[] args) {
 		System.out.println("this is the webdriver utlity");
-		System.out.println("my name is sumit");
 		System.out.println("Hi==>Anjali");
+		System.out.println("my name is sumit");
 	}
 
 }
