bdc4308c659b0fde73c6e79c955dac21a8fd6793
anjal <anjal@ANCHAL>
2025-03-12 12:22:47 +0530

    Added sopln statement to webdriver utility

--- myproject/src/main/java/com/javautlity/WebdriverUtlity.java
+++ myproject/src/main/java/com/javautlity/WebdriverUtlity.java
@@ -3,6 +3,7 @@ package com.javautlity;
 public class WebdriverUtlity {
 	public static void main(String[] args) {
 		System.out.println("this is the webdriver utlity");
+		System.out.println("Hi==>Anjali");
 	}
 
 }
