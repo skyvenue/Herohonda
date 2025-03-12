b9e5901564121b995e3f3571287a6e0848bec620
harivardhanreddykonikanti <harivardhanreddyking@gmail.com>
2025-03-12 13:19:45 +0530

    in java utlity i am adding random number

--- myproject/src/main/java/com/javautlity/JavaUtlity.java
+++ myproject/src/main/java/com/javautlity/JavaUtlity.java
@@ -1,5 +1,12 @@
 package com.javautlity;
 
+import java.util.Random;
+
 public class JavaUtlity {
+	public static int random() {
+		Random random=new Random();
+		int num = random.nextInt();
+		return num;//this method is created by harivardhan
+	}
 
 }
