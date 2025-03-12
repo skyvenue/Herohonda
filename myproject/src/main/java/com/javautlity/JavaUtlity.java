58393ba426ac9c3c893feee2ec4cff8760c05cb4
sumit <sumit@Sneh>
2025-03-12 13:17:54 +0530

    Added random number by sumit.

--- myproject/src/main/java/com/javautlity/JavaUtlity.java
+++ myproject/src/main/java/com/javautlity/JavaUtlity.java
@@ -1,5 +1,15 @@
 package com.javautlity;
 
+import java.util.Random;
+
 public class JavaUtlity {
+	
+	public int getRandomNumber() {
+		  
+		  Random ranDom = new Random();
+		  int ranDomNumber = ranDom.nextInt(5000);
+		  return ranDomNumber;
+		  
+	  }
 
 }
