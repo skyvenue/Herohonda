32473beed1f9f0a645f4c90ea511231e9b9ae2ca
harivardhanreddykonikanti <harivardhanreddyking@gmail.com>
2025-03-12 15:14:47 +0530

    entire utlity

---
+++ myproject/src/main/java/com/genricUtlity/DataBaseUtility.java
@@ -0,0 +1,10 @@
+package com.genricUtlity;
+
+public class DataBaseUtility {
+	public static void main(String[] args) {
+		
+		System.out.println("This is DataBaseUtility");
+       System.out.println("This is created  by vishnu");
+	}
+
+}
---
+++ myproject/src/main/java/com/genricUtlity/WebdriverUtlity.java
@@ -0,0 +1,11 @@
+package com.genricUtlity;
+
+public class WebdriverUtlity {
+	public static void main(String[] args) {
+		System.out.println("this is the webdriver utlity");
+		System.out.println("This line is created by vishnu");
+		System.out.println("Hi==>Anjali");
+		System.out.println("my name is sumit");
+	}
+
+}
