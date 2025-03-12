c3e9b40e89bfd66698abc8157df9c715e74ae4cd
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-12 12:51:14 +0530

    DataBaseUtility is added

--- myproject/src/main/java/com/javautlity/DataBaseUtility.java
+++ myproject/src/main/java/com/javautlity/DataBaseUtility.java
@@ -1,11 +1,10 @@
 package com.javautlity;
 
 public class DataBaseUtility {
-
 	public static void main(String[] args) {
 		
 		System.out.println("This is DataBaseUtility");
-
+       System.out.println("This is created  by vishnu");
 	}
 
 }
